5b65e2ca32a5d31b048b2390432c820d59de74cb
vitoraa <vitor.lima@outlook.com>
2021-10-15 14:19:11 -0300

    mongodb conncetion

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './database/config/ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot(), RecipeModule],
+  imports: [ConfigModule.forRoot(),
+  TypeOrmModule.forRoot(ormConfig()),
+    RecipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
