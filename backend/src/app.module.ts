7b916b868be6eb76d14e78b619b3c565a43e8139
vitoraa <vitor.lima@outlook.com>
2021-10-15 13:02:51 -0300

    new module recipe

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [],
+  imports: [RecipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
