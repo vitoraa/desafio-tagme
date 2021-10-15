fb7f1da7003c04e84e9a32b47db003d9f9962935
vitoraa <vitor.lima@outlook.com>
2021-10-15 16:04:29 -0300

    setting api prefix to api/v1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { APIPrefix } from './constant/common';
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(APIPrefix.Version);
   const port = parseInt(process.env.SERVER_PORT);
   await app.listen(port);
 }
-bootstrap(); 
+bootstrap();
