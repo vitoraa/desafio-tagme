babe75d57905361b18ed448d57bcbce518454a58
vitoraa <vitor.lima@outlook.com>
2021-10-17 19:57:49 -0300

    customizing header component

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'Tagme'
 
-  ngOnInit(): void {
+  constructor () { }
+
+  ngOnInit (): void {
   }
 
 }
