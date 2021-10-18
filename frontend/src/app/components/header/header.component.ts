33356dd9db698fca62f70807c3526bc36c5c341b
vitoraa <vitor.lima@outlook.com>
2021-10-18 19:55:13 -0300

    Revert "customizing header component"
    This reverts commit babe75d57905361b18ed448d57bcbce518454a58.

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -7,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title: string = 'Tagme'
+  constructor() { }
 
-  constructor () { }
-
-  ngOnInit (): void {
+  ngOnInit(): void {
   }
 
 }
