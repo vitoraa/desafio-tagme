a23ea157d99a08ef675d333b5b849e281ac4de6e
vitoraa <vitor.lima@outlook.com>
2021-10-19 19:09:07 -0300

    enabling cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { APIPrefix } from './constant/common';
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(APIPrefix.Version);
+  app.enableCors();
   const port = parseInt(process.env.SERVER_PORT);
   await app.listen(port);
 }
