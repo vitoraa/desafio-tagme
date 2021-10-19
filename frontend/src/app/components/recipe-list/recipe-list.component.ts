3f55ee722866e0d74e9408c800871e22a89b62ea
vitoraa <vitor.lima@outlook.com>
2021-10-19 18:39:34 -0300

    adding recipe list and iterating each recipe in the html

--- frontend/src/app/components/recipe-list/recipe-list.component.ts
+++ frontend/src/app/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../entities/Recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[]
 
-  ngOnInit(): void {
+  constructor () { }
+
+  ngOnInit (): void {
   }
 
 }
