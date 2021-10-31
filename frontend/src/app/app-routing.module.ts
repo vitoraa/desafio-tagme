c2d2c6d5ee283e4abaf05367864e371c07088ac2
vitoraa <vitor.lima@outlook.com>
2021-10-31 18:16:01 -0300

    adding RecipeDetail route with id param

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'receitas', component: RecipesComponent },
+  { path: 'receitas/:id', component: RecipeDetailComponent },
 ];
 
 @NgModule({
--- frontend/src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ frontend/src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor (private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit () {
+    var id = this.route.snapshot.paramMap.get('id')
+    console.log(id)
   }
 
 }
