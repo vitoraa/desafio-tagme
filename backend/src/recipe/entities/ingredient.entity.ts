077067c030403430d323867d1223b31521b053be
vitoraa <vitor.lima@outlook.com>
2021-11-01 11:02:26 -0300

    adding id columns

--- backend/src/recipe/entities/ingredient.entity.ts
+++ backend/src/recipe/entities/ingredient.entity.ts
@@ -1,6 +1,9 @@
 import { Column, Entity, ObjectIdColumn } from "typeorm";
 
 export class Ingredient {
+  @ObjectIdColumn()
+  id: number;
+
   @Column({ type: 'string' })
   name: string;
 
--- backend/src/recipe/entities/step.entity.ts
+++ backend/src/recipe/entities/step.entity.ts
@@ -1,6 +1,9 @@
 import { Column, Entity, ObjectIdColumn } from "typeorm";
 
 export class Step {
+  @ObjectIdColumn()
+  id: number;
+
   @Column({ type: 'number' })
   order: number;
 
