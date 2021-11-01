29905d376be683bb1e44b3c19cde487ae423c40c
vitoraa <vitor.lima@outlook.com>
2021-11-01 11:12:24 -0300

    receiving recipe as param and creating basi strcuture

--- frontend/src/app/components/recipes/recipe-detail/recipe-detail-steps/recipe-detail-steps.component.ts
+++ frontend/src/app/components/recipes/recipe-detail/recipe-detail-steps/recipe-detail-steps.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../../../entities/Recipe';
 
 @Component({
   selector: 'app-recipe-detail-steps',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailStepsComponent implements OnInit {
 
-  constructor() { }
+  @Input() recipe: Recipe
 
-  ngOnInit(): void {
+  constructor () { }
+
+  ngOnInit (): void {
   }
 
 }
