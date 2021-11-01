9083aaa3393e18d979d51e46c100c05926a24d0b
vitoraa <vitor.lima@outlook.com>
2021-11-01 11:53:50 -0300

    getting steps selecteds

--- frontend/src/app/components/recipes/recipe-detail/recipe-detail-steps/recipe-detail-steps.component.ts
+++ frontend/src/app/components/recipes/recipe-detail/recipe-detail-steps/recipe-detail-steps.component.ts
@@ -9,10 +9,25 @@ import { Recipe } from '../../../../entities/Recipe';
 export class RecipeDetailStepsComponent implements OnInit {
 
   @Input() recipe: Recipe
+  selectedSteps: number[] = []
 
   constructor () { }
 
   ngOnInit (): void {
+  }
+
+  updateSelectedOrders (event: Event, orderStep: number): void {
+    const isChecked = (<HTMLInputElement>event.target).checked
+    if (isChecked) {
+      this.selectedSteps.push(orderStep)
+    } else {
+      this.selectedSteps = this.selectedSteps.filter(step => step !== orderStep)
+    }
+    console.log(this.selectedSteps)
+  }
+
+  isAllStepsSelected (): boolean {
+    return this.selectedSteps.length === this.recipe.steps.length
   }
 
 }
