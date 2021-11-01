b0c3526121f22b35ec1f7847af4bb960194daba2
vitoraa <vitor.lima@outlook.com>
2021-11-01 11:03:04 -0300

    creating Ingredient and Step models

--- frontend/src/app/entities/Recipe.ts
+++ frontend/src/app/entities/Recipe.ts
@@ -1,7 +1,12 @@
+import { Ingredient } from "./Ingredient";
+import { Step } from "./Step";
+
 export interface Recipe {
   id: string
   name: string,
   description: string
   minutesToPrepare: number
   image: string
+  ingredients: Ingredient[]
+  steps: Step[]
 }
---
+++ frontend/src/app/entities/Step.ts
@@ -0,0 +1,5 @@
+export interface Step {
+  id: string
+  order: number,
+  description: string
+}
