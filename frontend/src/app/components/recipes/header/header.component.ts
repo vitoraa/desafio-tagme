703c3d61811f9333819212f0a9db59a0ba6f26c0
vitoraa <vitor.lima@outlook.com>
2021-11-02 22:04:06 -0300

    logout function

--- frontend/src/app/components/recipes/header/header.component.ts
+++ frontend/src/app/components/recipes/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { faUtensils, faList, faUser } from '@fortawesome/free-solid-svg-icons'
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -10,16 +12,23 @@ export class HeaderComponent implements OnInit {
   faUser = faUser
   faUtensils = faUtensils
   faList = faList
+
   @Output() onSearchRecipe: EventEmitter<string> = new EventEmitter()
   searchText: string = ''
 
-  constructor () { }
+  constructor (private loginService: LoginService, private router: Router) { }
 
   ngOnInit (): void {
   }
 
   onSubmitSearch (): void {
     this.onSearchRecipe.emit(this.searchText);
+  }
+
+  logout (): void {
+    this.loginService.logout()
+    this.router.navigate(['/'])
+    console.log('logout')
   }
 
 }
--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -7,16 +7,22 @@ import { Login } from '../components/login/shared/login';
 export class LoginService {
 
   loggedUser: Login
+  logged: boolean = false
 
   constructor () { }
 
-  login (login: Login) {
+  login (login: Login): boolean {
     var { userName, password } = login
     if (userName === 'admin' && password === 'admin') {
       this.loggedUser = login
+      this.logged = true
       return true
     }
     return false
+  }
+
+  logout () {
+    this.logged = false
   }
 
   isAuthenticated () {
